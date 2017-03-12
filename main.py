4d240b2a72bbfdf9badb7cd263f5ef95277879e7
Kelvin Jiang <kelvin.8000@gmail.com>
2017-03-12 16:59:50 -0700

    intermediate push

--- main.py
+++ main.py
@@ -30,3 +30,23 @@ def login():
     password = request.headers.get('password')
     auth = firebase.auth()
     auth.sign_in_with_email_and_password(username, password)
+
+@app.route('/createlist')
+def create_list():
+    owner = request.headers.get('owner')
+    list_name = request.headers.get('name')
+
+    db = firebase.database()
+    new_list_key =  db.generate_key()
+    print new_list_key
+    list_data = {
+        'owner': owner,
+        'name': list_name
+    }
+#{'owner': str(owner),'name': str(list_name)}
+    # db.child("lists").push(new_list_key)
+    db.child("lists").push(list_data)
+    # db.update(list_data)
+    # db.child("lists").child(new_list_key).set(list_data)
+
+    return new_list_key
