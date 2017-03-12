7b0d6c9ac77105ad6ec888ad16b789dcd944bbba
Kelvin Jiang <kelvin.8000@gmail.com>
2017-03-12 16:35:34 -0700

    implement create user + login

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask import request
-from firebase import firebase
+import pyrebase
 
 app = Flask(__name__)
+
+config = {
+  "apiKey": "AIzaSyBXZpwPptJjUJddDl6Q0DpG_3k7mGDdHk8",
+  "authDomain": "my-first-project-fea9c.firebaseapp.com",
+  "databaseURL": "https://my-first-project-fea9c.firebaseio.com/",
+  "storageBucket": "my-first-project-fea9c.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
 
 @app.route('/')
 def hello_world():
@@ -10,5 +19,14 @@ def hello_world():
 
 @app.route('/createuser')
 def create_user():
-    username = request.args.get('username')
-    password = request.args.get('password')
+    username = request.headers.get('email')
+    password = request.headers.get('password')
+    auth = firebase.auth()
+    auth.create_user_with_email_and_password(username, password)
+
+@app.route('/login')
+def login():
+    username = request.headers.get('email')
+    password = request.headers.get('password')
+    auth = firebase.auth()
+    auth.sign_in_with_email_and_password(username, password)
